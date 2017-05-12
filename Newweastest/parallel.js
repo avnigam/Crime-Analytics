7f8d0b5ec8c341dad87ec4fce9072e77aef5055f
avnigam <avnigam@cs.stonybrook.edu>
2017-05-12 04:27:16 -0400

    Commit

---
+++ Newweastest/parallel.js
@@ -0,0 +1,187 @@
+var global_year_parallel = 0;
+var global_state_parallel = "";
+
+function parallel_plot(plotdata, state, year) {
+var margin = {top: 30, right: 100, bottom: 20, left: 100},
+		width = 1500 - margin.left - margin.right,
+		height = 500 - margin.top - margin.bottom;
+
+	var dimensions = [
+	  {
+		name: "victim sex",
+		scale: d3.scale.ordinal().rangePoints([0, height]),
+		type: "string"
+	  },
+	  {
+		name: "victim age",
+		scale: d3.scale.linear().range([0, height]),
+		type: "number"
+	  },
+	  {
+		name: "victim race",
+		scale: d3.scale.ordinal().rangePoints([0, height]),
+		type: "string"
+	  },
+	  {
+		name: "relationship",
+		scale: d3.scale.ordinal().rangePoints([0, height]),
+		type: "string"
+	  },
+	  {
+		name: "perpetrator race",
+		scale: d3.scale.ordinal().rangePoints([0, height]),
+		type: "string"
+	  },
+	  {
+		name: "perpetrator age",
+		scale: d3.scale.linear().range([0, height]),
+		type: "number"
+	  },
+	  {
+		name: "perpetrator sex",
+		scale: d3.scale.ordinal().rangePoints([0, height]),
+		type: "string"
+	  },
+	];
+
+	var x = d3.scale.ordinal()
+		.domain(dimensions.map(function(d) { return d.name; }))
+		.rangePoints([0, width,+15]);
+
+	var line = d3.svg.line()
+		.defined(function(d) { return !isNaN(d[1]); });
+
+	var yAxis = d3.svg.axis()
+		.orient("left");
+
+	var svg = d3.select("#parallel_chart").append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+	  .append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+		
+	svg.append("text")
+        .attr("x", (width / 2))             
+        .attr("y", 0 - (margin.top / 2))
+        .attr("text-anchor", "middle")  
+        .style("font-size", "16px") 
+        .style("text-decoration", "bold")  
+        .text("State: " + state + " - Year: " + year);
+
+	var dimension = svg.selectAll(".dimension")
+		.data(dimensions)
+	  .enter().append("g")
+		.attr("class", "dimension")
+		.attr("transform", function(d) { return "translate(" + x(d.name) + ")"; });
+
+	var data = d3.csv.parse(plotdata, function (d) {
+        return d;
+    });
+	  dimensions.forEach(function(dimension) {
+		dimension.scale.domain(dimension.type === "number"
+			? d3.extent(data, function(d) { return +d[dimension.name]; })
+			: data.map(function(d) { return d[dimension.name]; }).sort());
+	  });
+
+	  svg.append("g")
+		  .attr("class", "background")
+		.selectAll("path")
+		  .data(data)
+		.enter().append("path")
+		  .attr("d", draw);
+
+	  svg.append("g")
+		  .attr("class", "foreground")
+		.selectAll("path")
+		  .data(data)
+		.enter().append("path")
+		  .attr("d", draw);
+
+	  dimension.append("g")
+		  .attr("class", "axis")
+		  .each(function(d) { d3.select(this).call(yAxis.scale(d.scale)); })
+		.append("text")
+		  .attr("class", "title")
+		  .attr("text-anchor", "middle")
+		  .attr("y", -9)
+		  .text(function(d) { return d.name; });
+
+	  var ordinal_labels = svg.selectAll(".axis text")
+		  .on("mouseover", mouseover)
+		  .on("mouseout", mouseout);
+
+	  var projection = svg.selectAll(".background path,.foreground path")
+		  .on("mouseover", mouseover)
+		  .on("mouseout", mouseout);
+
+	  function mouseover(d) {
+		svg.classed("active", true);
+
+		// this could be more elegant
+		if (typeof d === "string") {
+		  projection.classed("inactive", function(p) { return p.name !== d; });
+		  projection.filter(function(p) { return p.name === d; }).each(moveToFront);
+		  ordinal_labels.classed("inactive", function(p) { return p !== d; });
+		  ordinal_labels.filter(function(p) { return p === d; }).each(moveToFront);
+		} else {
+		  projection.classed("inactive", function(p) { return p !== d; });
+		  projection.filter(function(p) { return p === d; }).each(moveToFront);
+		  ordinal_labels.classed("inactive", function(p) { return p !== d.name; });
+		  ordinal_labels.filter(function(p) { return p === d.name; }).each(moveToFront);
+		}
+	  }
+
+	  function mouseout(d) {
+		svg.classed("active", false);
+		projection.classed("inactive", false);
+		ordinal_labels.classed("inactive", false);
+	  }
+
+	  function moveToFront() {
+		this.parentNode.appendChild(this);
+	  }
+
+	function draw(d) {
+	  return line(dimensions.map(function(dimension) {
+		return [x(dimension.name), dimension.scale(d[dimension.name])];
+	  }));
+	}
+}
+
+function parallel_csv(objArray) {
+    var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+    var str = '';
+	str = 'perpetrator age,perpetrator race,perpetrator sex,relationship,victim age,victim race,victim sex' + '\r\n';
+
+    for (var i = 0; i < array.length; i++) {
+        var line = '';
+        for (var index in array[i]) {
+            if (line != '') line += ','
+
+            line += array[i][index];
+        }
+
+        str += line + '\r\n';
+    }
+
+    return str;
+}
+
+function parallel(state, year) {
+	d3.select("#parallel_chart").selectAll("svg").remove();
+	if (state == '') {
+		state = global_state_parallel;
+	} else {
+		global_state_parallel = state;
+	}
+	
+	if (year == 0) {
+		year = global_year_parallel;
+	} else {
+		global_year_parallel = year;
+	}
+    d3.json('http://localhost:5000/api/parallel/' + year + '/' + state, function (error, data) {
+        var csv = parallel_csv(data);
+        parallel_plot(csv, state, year);
+    });
+}
---
+++ Newweastest/st_p_age.asm
@@ -0,0 +1,534 @@
+New York,2004,18,14,51
+Missouri,2004,17,5,16
+Florida,2004,35,20,40
+Maryland,2004,19,9,21
+Ohio,2004,22,15,18
+South Carolina,2004,19,12,2
+Arizona,2004,21,5,36
+California,2004,96,48,124
+Georgia,2004,17,14,23
+Texas,2004,61,26,56
+Nevada,2004,5,3,7
+Pennsylvania,2004,19,21,27
+District of Columbia,2004,6,0,11
+Michigan,2004,9,18,33
+Alabama,2004,9,7,10
+New Jersey,2004,20,8,20
+Oklahoma,2004,9,6,6
+Virginia,2004,16,10,16
+Indiana,2004,11,5,14
+Mississippi,2004,4,2,10
+Arkansas,2004,2,6,3
+Wisconsin,2004,9,4,4
+Hawaii,2004,2,2,1
+Kentucky,2004,12,2,7
+North Carolina,2004,18,9,17
+South Dakota,2004,2,0,1
+Utah,2004,5,1,0
+Washington,2004,9,8,4
+Massachusetts,2004,8,5,5
+Illinois,2004,28,7,17
+Nebraska,2004,1,0,3
+Colorado,2004,7,10,3
+New Mexico,2004,11,5,2
+Tennessee,2004,12,15,10
+Louisiana,2004,17,6,36
+Oregon,2004,3,0,1
+Delaware,2004,0,1,1
+West Virginia,2004,2,1,0
+Kansas,2004,5,4,1
+Minnesota,2004,6,3,3
+Connecticut,2004,3,2,3
+Montana,2004,0,1,1
+Alaska,2004,1,2,1
+Iowa,2004,0,0,2
+Idaho,2004,0,0,2
+Rhodes Island,2004,0,0,1
+North Dakota,2004,1,0,0
+Maine,2004,0,2,0
+New Hampshire,2004,0,1,1
+Vermont,2004,1,1,1
+Wyoming,2004,1,0,0
+Arizona,2005,11,13,18
+Georgia,2005,14,17,18
+Louisiana,2005,27,3,20
+Washington,2005,13,8,5
+California,2005,89,48,98
+Missouri,2005,21,6,13
+Florida,2005,32,31,46
+Ohio,2005,18,17,20
+Oklahoma,2005,14,6,5
+Texas,2005,73,40,37
+Illinois,2005,21,7,34
+Maryland,2005,21,4,33
+Indiana,2005,17,13,17
+Michigan,2005,18,11,28
+District of Columbia,2005,0,0,17
+Kentucky,2005,7,7,6
+Alabama,2005,17,6,16
+New Jersey,2005,25,4,20
+Pennsylvania,2005,36,14,38
+Massachusetts,2005,4,1,10
+Alaska,2005,2,0,1
+New York,2005,25,19,48
+Virginia,2005,30,8,12
+Nevada,2005,9,4,8
+South Carolina,2005,11,9,5
+Utah,2005,6,3,2
+Kansas,2005,5,5,2
+North Carolina,2005,28,10,16
+Minnesota,2005,5,3,3
+Idaho,2005,3,1,1
+Colorado,2005,4,5,3
+Tennessee,2005,18,18,9
+Wisconsin,2005,10,7,8
+Oregon,2005,4,1,3
+Connecticut,2005,8,3,2
+Arkansas,2005,8,5,4
+Mississippi,2005,3,5,3
+Montana,2005,2,0,0
+New Mexico,2005,5,3,0
+West Virginia,2005,4,1,1
+Hawaii,2005,1,0,0
+Rhodes Island,2005,1,0,2
+New Hampshire,2005,1,1,1
+Iowa,2005,0,2,1
+Wyoming,2005,1,0,1
+Delaware,2005,0,1,1
+Maine,2005,0,0,1
+Michigan,2006,21,17,44
+California,2006,101,43,116
+South Carolina,2006,22,15,4
+Indiana,2006,16,7,9
+New Hampshire,2006,1,0,0
+Tennessee,2006,17,17,13
+Oklahoma,2006,8,11,4
+Alabama,2006,15,9,16
+Illinois,2006,22,5,30
+Nevada,2006,7,5,9
+Pennsylvania,2006,39,19,23
+Texas,2006,59,29,56
+New Jersey,2006,16,7,18
+Arizona,2006,15,11,19
+New York,2006,38,17,33
+Florida,2006,45,27,49
+Washington,2006,13,4,2
+Kentucky,2006,4,9,3
+Louisiana,2006,19,7,19
+Ohio,2006,22,9,16
+Colorado,2006,5,5,3
+Missouri,2006,11,9,16
+Virginia,2006,22,11,9
+North Carolina,2006,24,11,23
+Delaware,2006,2,3,1
+Alaska,2006,1,3,2
+Arkansas,2006,8,5,9
+Oregon,2006,4,5,5
+Idaho,2006,3,2,0
+Maryland,2006,14,8,27
+Minnesota,2006,10,3,5
+Wisconsin,2006,12,6,6
+Iowa,2006,2,2,0
+Georgia,2006,22,13,29
+Massachusetts,2006,5,3,10
+Mississippi,2006,14,0,2
+Kansas,2006,10,3,4
+North Dakota,2006,1,1,0
+District of Columbia,2006,1,1,8
+Connecticut,2006,3,6,5
+New Mexico,2006,4,2,2
+South Dakota,2006,0,0,1
+Rhodes Island,2006,0,4,1
+Nebraska,2006,1,1,0
+West Virginia,2006,2,5,1
+Montana,2006,1,1,2
+Maine,2006,0,1,0
+Utah,2006,3,0,0
+Hawaii,2006,0,1,0
+District of Columbia,2007,0,0,17
+North Carolina,2007,25,13,18
+Oregon,2007,4,1,6
+Pennsylvania,2007,26,11,28
+Missouri,2007,12,12,22
+Maryland,2007,16,12,30
+Arizona,2007,21,8,16
+Florida,2007,43,38,60
+Illinois,2007,14,7,30
+Georgia,2007,21,13,26
+Texas,2007,55,52,57
+Idaho,2007,2,0,2
+Washington,2007,10,5,5
+Oklahoma,2007,8,6,5
+New Jersey,2007,19,7,22
+Arkansas,2007,5,7,5
+New York,2007,36,10,31
+Alaska,2007,4,1,0
+New Mexico,2007,4,0,3
+West Virginia,2007,2,3,1
+Indiana,2007,13,13,26
+Louisiana,2007,18,10,19
+Ohio,2007,18,12,15
+Mississippi,2007,6,4,3
+Tennessee,2007,28,14,15
+South Carolina,2007,24,5,6
+Virginia,2007,21,12,14
+Alabama,2007,14,9,15
+California,2007,86,48,118
+Michigan,2007,22,19,15
+Maine,2007,3,2,0
+Iowa,2007,1,1,1
+Nevada,2007,9,6,8
+Minnesota,2007,10,2,0
+Kansas,2007,6,4,5
+Hawaii,2007,2,1,0
+Massachusetts,2007,6,4,8
+Wyoming,2007,2,0,0
+Colorado,2007,8,7,7
+Connecticut,2007,3,4,4
+Wisconsin,2007,4,5,3
+Kentucky,2007,9,3,4
+Delaware,2007,3,2,0
+North Dakota,2007,3,0,0
+Montana,2007,1,0,0
+Rhodes Island,2007,1,0,0
+Utah,2007,4,1,0
+Vermont,2007,0,1,0
+Nebraska,2007,1,2,0
+South Dakota,2007,2,1,0
+New Hampshire,2007,0,0,1
+Florida,2008,53,31,53
+Alabama,2008,17,7,12
+New York,2008,39,19,43
+North Carolina,2008,25,21,24
+Georgia,2008,25,12,22
+South Carolina,2008,20,5,10
+California,2008,54,32,101
+New Mexico,2008,8,2,6
+Maryland,2008,18,7,30
+New Jersey,2008,11,6,15
+Pennsylvania,2008,19,18,32
+Louisiana,2008,19,4,22
+Kentucky,2008,4,4,6
+Ohio,2008,18,11,20
+Texas,2008,60,30,43
+Washington,2008,10,5,8
+Arizona,2008,17,9,19
+Mississippi,2008,5,6,6
+West Virginia,2008,4,2,5
+Tennessee,2008,21,12,10
+Michigan,2008,20,5,36
+Connecticut,2008,6,1,4
+Hawaii,2008,0,3,0
+Montana,2008,1,0,0
+Oklahoma,2008,8,7,9
+Virginia,2008,11,13,5
+Missouri,2008,13,6,25
+Delaware,2008,2,0,0
+Nevada,2008,5,1,9
+Illinois,2008,7,2,37
+Idaho,2008,3,1,0
+Colorado,2008,3,6,3
+Indiana,2008,12,1,16
+Iowa,2008,4,5,2
+South Dakota,2008,1,2,0
+Arkansas,2008,6,6,4
+Massachusetts,2008,6,2,5
+Kansas,2008,4,0,1
+Utah,2008,2,2,2
+Nebraska,2008,2,1,0
+Oregon,2008,3,5,0
+Maine,2008,1,0,1
+Wisconsin,2008,8,1,0
+Vermont,2008,1,3,0
+Minnesota,2008,7,3,1
+Alaska,2008,0,2,0
+New Hampshire,2008,1,2,0
+Georgia,2009,30,13,30
+California,2009,57,55,106
+Louisiana,2009,20,11,18
+Ohio,2009,27,9,17
+Colorado,2009,10,3,3
+Tennessee,2009,23,13,9
+Texas,2009,59,33,42
+New York,2009,22,13,34
+Michigan,2009,17,14,33
+Florida,2009,30,39,53
+Maryland,2009,14,10,18
+Oklahoma,2009,12,6,4
+New Mexico,2009,15,5,4
+Nevada,2009,6,7,4
+Arkansas,2009,4,4,5
+Delaware,2009,2,0,1
+Virginia,2009,12,12,5
+Maine,2009,2,3,2
+Wisconsin,2009,10,4,3
+Idaho,2009,1,0,0
+Mississippi,2009,10,4,3
+Alabama,2009,6,8,16
+Rhodes Island,2009,0,3,3
+Pennsylvania,2009,22,25,35
+Missouri,2009,11,11,19
+Indiana,2009,12,10,18
+New Jersey,2009,5,3,18
+Utah,2009,0,3,0
+Washington,2009,13,8,4
+South Carolina,2009,17,8,7
+North Carolina,2009,12,15,19
+Illinois,2009,5,4,42
+Kentucky,2009,6,8,3
+Iowa,2009,4,3,0
+Arizona,2009,8,12,14
+Minnesota,2009,5,3,2
+Hawaii,2009,1,2,1
+Kansas,2009,5,4,4
+District of Columbia,2009,7,2,11
+South Dakota,2009,0,1,0
+Nebraska,2009,0,3,1
+Massachusetts,2009,10,6,6
+Oregon,2009,3,4,3
+West Virginia,2009,1,4,0
+Alaska,2009,2,0,1
+Connecticut,2009,2,2,1
+Montana,2009,0,2,0
+New Hampshire,2009,1,0,2
+Virginia,2010,16,14,9
+New Jersey,2010,17,5,16
+California,2010,67,37,109
+Ohio,2010,26,17,20
+Texas,2010,52,41,36
+South Carolina,2010,24,13,8
+Missouri,2010,20,10,13
+Georgia,2010,36,20,18
+Kentucky,2010,8,10,3
+Pennsylvania,2010,23,14,31
+New York,2010,40,13,34
+Michigan,2010,26,15,32
+Wisconsin,2010,7,5,2
+Tennessee,2010,14,16,8
+Arizona,2010,13,14,18
+Utah,2010,0,4,2
+Florida,2010,29,20,46
+District of Columbia,2010,5,2,4
+Louisiana,2010,10,5,17
+North Carolina,2010,18,20,11
+Illinois,2010,10,4,35
+Maryland,2010,13,4,19
+Nevada,2010,7,1,3
+Washington,2010,6,3,6
+Mississippi,2010,9,7,6
+Alabama,2010,6,5,7
+Oklahoma,2010,12,4,4
+Delaware,2010,3,1,1
+Nebraska,2010,1,0,3
+Connecticut,2010,2,1,7
+Indiana,2010,13,7,20
+Kansas,2010,3,4,4
+Idaho,2010,1,2,0
+Massachusetts,2010,5,1,6
+New Mexico,2010,5,1,2
+Minnesota,2010,2,1,4
+Montana,2010,1,1,0
+Colorado,2010,3,6,3
+Arkansas,2010,1,4,3
+Maine,2010,3,1,1
+North Dakota,2010,1,0,0
+Alaska,2010,1,3,3
+Oregon,2010,3,1,0
+Hawaii,2010,1,2,1
+Iowa,2010,2,0,1
+West Virginia,2010,1,2,0
+South Dakota,2010,0,2,0
+New Hampshire,2010,1,0,0
+Rhodes Island,2010,1,0,0
+Texas,2011,51,41,43
+Ohio,2011,23,16,17
+California,2011,49,44,86
+Virginia,2011,17,14,6
+Pennsylvania,2011,21,10,32
+Florida,2011,42,28,36
+Nebraska,2011,1,0,2
+South Carolina,2011,19,13,8
+New Jersey,2011,18,7,24
+Maine,2011,0,2,0
+Wisconsin,2011,12,5,2
+Georgia,2011,19,13,25
+North Carolina,2011,16,14,14
+Kentucky,2011,6,5,6
+New York,2011,28,14,32
+Tennessee,2011,24,11,12
+Mississippi,2011,11,8,6
+Louisiana,2011,20,5,23
+Maryland,2011,17,9,19
+Missouri,2011,13,14,15
+Minnesota,2011,2,3,2
+Massachusetts,2011,7,2,4
+Arkansas,2011,7,7,7
+Utah,2011,2,1,2
+Indiana,2011,15,11,6
+Arizona,2011,17,13,12
+Oregon,2011,6,3,4
+Nevada,2011,7,7,4
+New Mexico,2011,6,4,5
+Illinois,2011,9,1,28
+Oklahoma,2011,4,5,5
+Colorado,2011,8,7,4
+Michigan,2011,12,10,28
+Kansas,2011,7,1,2
+West Virginia,2011,8,4,0
+Alaska,2011,1,2,0
+Washington,2011,11,7,9
+South Dakota,2011,0,3,0
+District of Columbia,2011,3,2,2
+Iowa,2011,5,2,2
+Wyoming,2011,3,2,0
+Connecticut,2011,5,0,6
+Idaho,2011,1,2,0
+Delaware,2011,1,0,1
+Hawaii,2011,2,0,0
+Texas,2012,58,37,34
+Arizona,2012,12,11,9
+North Carolina,2012,22,16,22
+Tennessee,2012,22,16,11
+Michigan,2012,26,19,38
+New York,2012,17,15,31
+Massachusetts,2012,6,4,10
+Illinois,2012,10,6,35
+Florida,2012,36,39,48
+California,2012,72,45,93
+Ohio,2012,21,12,10
+Arkansas,2012,4,5,7
+Oklahoma,2012,11,2,6
+South Carolina,2012,19,13,10
+New Jersey,2012,15,9,13
+Georgia,2012,23,10,25
+Louisiana,2012,16,6,21
+Virginia,2012,16,14,8
+Washington,2012,13,12,1
+New Hampshire,2012,3,0,1
+Iowa,2012,3,3,1
+Kentucky,2012,11,11,6
+Kansas,2012,1,4,0
+Pennsylvania,2012,24,15,28
+Oregon,2012,2,3,3
+Indiana,2012,10,4,12
+Nevada,2012,7,5,4
+Idaho,2012,1,1,0
+Connecticut,2012,4,0,6
+Utah,2012,2,2,0
+Mississippi,2012,8,4,3
+Colorado,2012,7,8,4
+Rhodes Island,2012,0,3,1
+Delaware,2012,3,1,0
+Maryland,2012,11,7,20
+Nebraska,2012,2,1,4
+Hawaii,2012,1,2,1
+Wisconsin,2012,6,3,3
+Missouri,2012,16,7,10
+West Virginia,2012,2,3,1
+Minnesota,2012,8,2,2
+Alaska,2012,3,1,2
+Montana,2012,0,1,1
+New Mexico,2012,8,2,1
+South Dakota,2012,1,0,0
+Vermont,2012,0,1,0
+North Dakota,2012,1,1,0
+Maine,2012,0,2,0
+Wyoming,2012,0,1,0
+Wisconsin,2013,12,7,3
+Georgia,2013,27,16,14
+Michigan,2013,16,18,32
+California,2013,73,45,92
+Tennessee,2013,13,17,9
+Pennsylvania,2013,18,7,33
+Washington,2013,13,6,2
+Texas,2013,42,42,40
+District of Columbia,2013,5,4,7
+Oregon,2013,1,3,2
+Illinois,2013,11,1,35
+Florida,2013,34,24,37
+Arizona,2013,19,11,14
+Virginia,2013,14,12,8
+New Jersey,2013,17,8,16
+New Mexico,2013,7,4,3
+Indiana,2013,17,2,19
+Maryland,2013,11,6,29
+Arkansas,2013,6,4,5
+South Carolina,2013,15,12,8
+Nevada,2013,11,4,6
+Connecticut,2013,2,3,3
+New York,2013,18,12,18
+Maine,2013,0,2,1
+Louisiana,2013,12,6,23
+North Carolina,2013,19,9,23
+Delaware,2013,2,0,1
+Kentucky,2013,6,6,5
+Colorado,2013,6,8,5
+West Virginia,2013,2,6,0
+Missouri,2013,13,6,18
+Oklahoma,2013,8,12,5
+Mississippi,2013,8,3,2
+Ohio,2013,19,9,20
+New Hampshire,2013,1,0,1
+Massachusetts,2013,5,0,8
+Kansas,2013,6,2,1
+Montana,2013,1,1,0
+Utah,2013,3,6,0
+Nebraska,2013,4,2,1
+Minnesota,2013,7,3,4
+Rhodes Island,2013,3,0,0
+Alaska,2013,2,0,0
+Vermont,2013,1,1,0
+Iowa,2013,0,3,1
+Wyoming,2013,0,2,0
+South Dakota,2013,1,0,0
+Idaho,2013,0,1,0
+Texas,2014,43,31,45
+New York,2014,21,15,27
+Idaho,2014,1,1,0
+Georgia,2014,28,10,25
+Ohio,2014,20,12,16
+Florida,2014,41,35,43
+California,2014,66,39,87
+Rhodes Island,2014,1,2,0
+Utah,2014,3,1,0
+Tennessee,2014,22,10,11
+Michigan,2014,29,9,20
+Delaware,2014,4,0,3
+District of Columbia,2014,5,2,8
+New Jersey,2014,9,3,17
+South Carolina,2014,11,13,7
+Colorado,2014,9,4,3
+Washington,2014,7,11,4
+Wisconsin,2014,6,6,2
+Arizona,2014,8,10,12
+Nevada,2014,3,3,16
+Missouri,2014,15,8,24
+Louisiana,2014,19,8,14
+Virginia,2014,15,9,9
+Illinois,2014,12,4,39
+Maryland,2014,10,7,19
+Mississippi,2014,10,5,5
+Indiana,2014,15,8,14
+Massachusetts,2014,0,4,5
+Oregon,2014,0,3,4
+North Carolina,2014,15,8,21
+Arkansas,2014,10,9,3
+Oklahoma,2014,7,5,5
+Kansas,2014,2,1,4
+Minnesota,2014,3,3,2
+Pennsylvania,2014,13,18,32
+Hawaii,2014,0,0,1
+West Virginia,2014,2,5,1
+Connecticut,2014,4,5,4
+Nebraska,2014,2,0,3
+Maine,2014,2,1,0
+Kentucky,2014,5,6,2
+North Dakota,2014,0,1,0
+New Mexico,2014,8,7,3
+Alaska,2014,2,1,1
+Iowa,2014,2,2,1
+South Dakota,2014,1,2,0
+Alabama,2014,1,0,0
+Wyoming,2014,1,0,0
+Montana,2014,1,3,0
+New Hampshire,2014,0,1,0
